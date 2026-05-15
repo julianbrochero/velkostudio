87101459dd43683bd10f494bf534848cc9eb5e6b
Julian Brochero <julianbrochero@users.noreply.github.com>
2026-05-15 20:54:17 -0300

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,23 @@
-import createMDX from '@next/mdx';
+import createMDX from '@next/mdx'
+
+const isGithubPages = process.env.GITHUB_PAGES === 'true'
+const githubPagesBasePath = '/velkostudio'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-};
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubPages ? githubPagesBasePath : undefined,
+  assetPrefix: isGithubPages ? `${githubPagesBasePath}/` : undefined,
+}
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
-});
+})
 
-export default withMDX(nextConfig);
+export default withMDX(nextConfig)
